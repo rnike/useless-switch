17f9605044ab04c0ae427854ed0b5d02230197fa
Mike <yum650350@gmail.com>
2022-05-09 23:26:21 +0800

    export gh-pages

---
+++ _next/static/Wl2wwyf1pYwJFUsZCyDJ2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/7547b1095dd9f0ec.css","static/chunks/pages/index-b41aaeb3fd602971.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
