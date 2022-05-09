e216e70d0e50a6291fc86d9974b4b17527be3ec3
Mike <yum650350@gmail.com>
2022-05-09 23:26:21 +0800

    export gh-pages

--- app/next.config.js
+++ app/next.config.js
@@ -1,6 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const prod = process.env.NODE_ENV === 'production';
 
-module.exports = nextConfig
+module.exports = {
+  exportPathMap: function () {
+    return {
+      '/': { page: '/' },
+    };
+  },
+  assetPrefix: prod ? '/useless-switch/' : '',
+  webpack: (config, { dev }) => {
+    // Perform customizations to webpack config
+    // console.log('webpack');
+    // console.log(config.module.rules, dev);
+    config.module.rules = config.module.rules.map((rule) => {
+      if (rule.loader === 'babel-loader') {
+        rule.options.cacheDirectory = false;
+      }
+      return rule;
+    });
+    // Important: return the modified config
+    return config;
+  },
+};
